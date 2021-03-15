2e4f2b66398017dd4d198194f3587ec570b6011f
dannauu <scatterman16@gmail.com>
2021-03-14 21:55:12 -0500

    added setup for jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
-// Sets up connection to SQL Database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'Jesse2020!',
-  database: 'burgersdb',
-});
+if (process.env.JAWSDB_URL) {
+  connection.mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Jesse2020!",
+    database: "burgersdb",
+  });
+}
 
 // Connects to database
 connection.connect((err) => {
